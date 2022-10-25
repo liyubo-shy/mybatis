ab7ba37b33da1a483ccdc217810f61b402a265af
liyubo-shy <859591603@qq.com>
2022-10-25 16:14:18 +0800

    mybatis

---
+++ src/main/java/com/bobo/MybatisTest2.java
@@ -0,0 +1,38 @@
+package com.bobo;
+
+import com.bobo.mapper.UserMapper;
+import com.bobo.pojo.Sys_user;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+/**
+ * @Author: bo
+ * @DATE: 2022/10/14 2:14
+ **/
+public class MybatisTest2 {
+    public static void main(String[] args) throws IOException {
+        //加载mybatis核心配置文件，获取SqlSessionFactory类
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+        //获取SqlSession对象，用于执行Sql
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        //执行Sql
+        //List<Sys_user> sysUser = sqlSession.selectList("com.bobo.mapper.UserMapper.selectAll");
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        List<Sys_user> sys_users = mapper.selectAll();
+
+        System.out.println(sys_users);
+
+        //释放资源
+        sqlSession.close();
+    }
+}
